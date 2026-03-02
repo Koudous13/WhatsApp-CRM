05523e0599a526fb391ca5d8767d4443bf6ed46f
Koudous <Koudousdaouda13@gmail.com>
2026-03-02 17:58:45 +0100

    chore: remove debug2 route

--- app/app/api/debug2/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-    const key = process.env.WASENDER_WEBHOOK_SECRET || ''
-    return NextResponse.json({
-        secretPrefix: key.substring(0, 5),
-        secretSuffix: key.substring(key.length - 5)
-    })
-}
