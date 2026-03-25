52e48d2e7a77adaa53023b9ac559190a554980f0
Koudous <Koudousdaouda13@gmail.com>
2026-03-25 15:37:32 +0100

    fix: syntax error in contacts/page.tsx

--- app/app/(dashboard)/contacts/page.tsx
+++ app/app/(dashboard)/contacts/page.tsx
@@ -366,7 +366,6 @@ export default function ContactsPage() {
                                 </div>
                             </div>
                         )}
-                        )}
                     </div>
                 </div>
 
